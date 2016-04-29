e3286e9a7577b5c534dd8758c2e06c577aae4377
cloner-hooshesiyah <loggerrin@yahoo.com>
2016-04-29 16:42:50 +0430

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -8,7 +8,7 @@ local function run(msg)
     local data = load_data(_config.moderation.data)
     if not data[tostring(msg.to.id)] then
       print "This is not our group. Leaving..."
-      chat_del_user('chat#id'..msg.to.id, 'user#id'..92307266, callback, false)
+      chat_del_user('chat#id'..msg.to.id, 'user#id'..167946378, callback, false)
     end
   end
 end
