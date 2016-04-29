f4ea97583394fea3d1e9eb88818a933b9baf6599
cloner-hooshesiyah <loggerrin@yahoo.com>
2016-04-29 09:54:14 -0700

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -1,23 +1,22 @@
-local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
-end
-
-local function run(msg)
-  if msg.service and msg.action.type == 'chat_add_user' then
-    local data = load_data(_config.moderation.data)
-    if not data[tostring(msg.to.id)] then
-      print "This is not our group. Leaving..."
-      chat_del_user('chat#id'..msg.to.id, 'user#id'..167946378, callback, false)
-    end
-  end
-end
-
+do local function run(msg, matches) 
+local user_bot = 110311925 
+local receiver = get_receiver(msg)
+    if matches[1] == 'sgergwr' and is_sudo(msg) then
+       channel_kick_user("channel#id"..msg.to.id, 'user#id'..user_bot, ok_cb, false)
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..user_bot, ok_cb, false)
+    leave(receiver, ok_cb, false)
+    elseif msg.service and msg.action.type == 'chat_add_user' or 'channel_invite_user' and msg.action.user.id == tonumber(user_bot) and not is_sudo(msg) then
+       send_large_msg(receiver, '', ok_cb, false)
+       chat_del_user(receiver, 'user#id'..user_bot, ok_cb, false)
+       channel_kick_user("channel#id"..msg.to.id, 'user#id'..user_bot, ok_cb, 
+false)
+    leave(receiver, ok_cb, false)
+    end end
+ 
 return {
-  description = "Kicking ourself (bot) from unmanaged groups.",
-  usage = "Plugin For Kiling add with id ....",
   patterns = {
-    "^!!tgservice (.+)$"
+    "^!!tgservice (.+)$",
   },
   run = run
 }
+end
